075ac56ad7ffdc0a71904800f3774682d1a051e9
Nikos Stylianou <68811067+nikosstyl@users.noreply.github.com>
2021-05-06 23:54:09 +0300

    Added some packets

--- setup.sh
+++ setup.sh
@@ -18,20 +18,22 @@ printf " Ubuntu Setup for UTh\n"
 echo "------------------------ STARTING PROCEDURE ------------------------"
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install build-essential gdb valgrind gcc
+sudo apt -y install build-essential gdb valgrind gcc git
 if [ $1 = "classic" ]
 then
+	sudo apt install -y kdocker
+	sudo snap install core
 	sudo snap install code --classic
-	sudo snap install teams-insider spotify
+	sudo snap install teams-insider
+	sudo snap install spotify
 	sudo apt -y install bc module-assistant dkms
 	sudo m-a prepare
 	cd ~
 	git clone https://github.com/tomaspinho/rtl8821ce.git
-	cd rtl8821ce
-	sudo ./dkms-install
+	cd rtl8821ce/
+	sudo ./dkms-install.sh
 elif [ $1 = "wsl" ]
 then
 	printf "Install VSCode from here: https://code.visualstudio.com/\n"
-	echo "End user can install whatever he/she wants!"
 fi
 echo "------------------------ ENDING PROCEDURE ------------------------"
